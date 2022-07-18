2594ceb666a7e8d0f3971b2ad7b2c50f7f489c0f
JasonHuang <lohas821019@gmail.com>
2022-07-18 20:34:35 +0800

    can post img

--- twse.py
+++ twse.py
@@ -10,9 +10,12 @@ import numpy as np
 import json
 import requests
 import time
-import dataframe_image as dfi
+# import dataframe_image as dfi
 import os
-import matplotlib as plt
+import matplotlib.pyplot as plt
+
+plt.rcParams['font.sans-serif'] = ['Microsoft JhengHei'] 
+plt.rcParams['axes.unicode_minus'] = False
 
 #pandas 結果對齊
 pd.set_option('display.unicode.ambiguous_as_wide', True)
@@ -53,11 +56,30 @@ def get_twse_trade():
         
         result = StockPrice.to_numpy()
         
-        if not os.path.exists(todaydate+'.jpg'):
-            dfi.export(StockPrice, './resources/'+todaydate+'.jpg')
+        fig,ax = render_mpl_table(StockPrice, header_columns=0, col_width=2.0)
+        fig.savefig('./resources/'+todaydate +".png")
+        # if not os.path.exists(todaydate+'.jpg'):
+        #     dfi.export(StockPrice, './resources/'+todaydate+'.jpg')
         return (200,todaydate,result)
     
+def render_mpl_table(data, col_width=3.0, row_height=0.625, font_size=14,
+                     header_color='#40466e', row_colors=['#f1f1f2', 'w'], edge_color='w',
+                     bbox=[0, 0, 1, 1], header_columns=0,
+                     ax=None, **kwargs):
+    if ax is None:
+        size = (np.array(data.shape[::-1]) + np.array([0, 1])) * np.array([col_width, row_height])
+        fig, ax = plt.subplots(figsize=size)
+        ax.axis('off')
+    mpl_table = ax.table(cellText=data.values, bbox=bbox, colLabels=data.columns, **kwargs)
+    mpl_table.auto_set_font_size(True)
+    mpl_table.set_fontsize(font_size)
 
-
-
+    for k, cell in mpl_table._cells.items():
+        cell.set_edgecolor(edge_color)
+        if k[0] == 0 or k[1] < header_columns:
+            cell.set_text_props(weight='bold', color='w')
+            cell.set_facecolor(header_color)
+        else:
+            cell.set_facecolor(row_colors[k[0]%len(row_colors) ])
+    return ax.get_figure(), ax
 
